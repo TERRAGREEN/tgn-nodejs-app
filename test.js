f92daefdad6a6c5eaad4e0e1040b6d5335da5f59
TERRA GREEN <44187811+TERRAGREEN@users.noreply.github.com>
2019-05-21 14:21:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const tgn = require('tgn-node-app');
 const app_tgn = new tgn({
   apiKey: process.env.COIN_API_KEY,
   secret: process.env.COIN_SECRET_KEY,
-  walletName: 'testsneha35',
-  password: 'Sneha@123'
+  walletName: '[wallet_name]',
+  password: '[password]'
 });
 app_tgn._AllRate(function(results){
     console.log(results);
